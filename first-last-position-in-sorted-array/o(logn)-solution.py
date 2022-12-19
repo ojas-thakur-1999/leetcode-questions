649a4e85b22b81746d1902ceaddfdc379edc983c
Ojas Thakur <ojasthakur1999@gmail.com>
2022-12-19 12:15:24 +0530

    first-last-position-in-sorted-array

--- first-last-position-in-sorted-array/o(logn)-solution.py
+++ first-last-position-in-sorted-array/o(logn)-solution.py
@@ -3,34 +3,10 @@ class Solution:
         leftIdx = self.binarySearchLeft(nums, target, 0, len(nums)-1)
         rightIdx = self.binarySearchRight(nums, target, 0, len(nums)-1)
         return [leftIdx, rightIdx]
-
-    def binarySearchRight(self, nums, target, start, end):
-        if end <= start:
-            if nums[start] == target:
-                return start
-            else:
-                return -1
-
-        right = -1
-        mid = (start+end) // 2
-        if target > nums[mid]:
-            right = self.binarySearchRight(nums, target, mid+1, end)
-        elif target < nums[mid]:
-            right = self.binarySearchRight(nums, target, start, mid-1)
-        else:
-            if nums[mid+1] == target:
-                right = self.binarySearchRight(nums, target, mid+1, end)
-            else:
-                right = mid
-        
-        return right
     
     def binarySearchLeft(self, nums, target, start, end):
-        if end <= start:
-            if nums[start] == target:
-                return start
-            else:
-                return -1
+        if end < start:
+            return -1
 
         left = -1
         mid = (start+end) // 2
@@ -39,10 +15,27 @@ class Solution:
         elif target < nums[mid]:
             left = self.binarySearchLeft(nums, target, start, mid-1)
         else:
-            if nums[mid-1] == target:
-                left = self.binarySearchLeft(nums, target, mid-1, end)
+            if mid > 0 and nums[mid-1] == target:
+                left = self.binarySearchLeft(nums, target, start, mid-1)
             else:
                 left = mid
         
         return left
 
+    def binarySearchRight(self, nums, target, start, end):
+            if end < start:
+                return -1
+
+            right = -1
+            mid = (start+end) // 2
+            if target > nums[mid]:
+                right = self.binarySearchRight(nums, target, mid+1, end)
+            elif target < nums[mid]:
+                right = self.binarySearchRight(nums, target, start, mid-1)
+            else:
+                if mid < len(nums)-1 and nums[mid+1] == target:
+                    right = self.binarySearchRight(nums, target, mid+1, end)
+                else:
+                    right = mid
+            
+            return right
---
+++ first-last-position-in-sorted-array/solution.md
@@ -0,0 +1,5 @@
+# Link to solutions
+https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/submissions/861973527/?envType=study-plan&id=algorithm-ii
+
+# Insights
+Using two separate recursion blocks
